8f23b00cdef2eb4ea52e1e9014b853cc5157d2d5
Arthur Ming <guobin.ming@pwc.com>
2022-09-13 10:07:59 +0800

    refactor: use crate to rewirte the config

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args).unwrap_or_else(|err| {
+    let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -15,7 +14,7 @@ fn main() {
     println!("In file {}", config.filename);
 
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
@@ -23,32 +22,4 @@ fn main() {
 }
 
 
-struct Config {
-    query: String,
-    filename: String,
-}
 
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("no enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn parse_config(args: &[String]) -> Result<Config, &'static str> {
-    return Config::new(args);    
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
